3e0c37d4c5f541694359aaf4aab5269ddbd37406
Davyd Abbasov <davidromashkov@gmail.com>
2025-11-24 17:58:05 +0100

    feat:fix migrations

--- internal/migrations/001_videos.up.sql
+++ internal/migrations/001_videos.up.sql
@@ -1,7 +1,7 @@
 -- Create tables
 CREATE TABLE IF NOT EXISTS videos (
     id SERIAL PRIMARY KEY,
-    tiktok_video_id TEXT NOT NULL UNIQUE,
+    tiktok_id TEXT NOT NULL UNIQUE,
     url TEXT NOT NULL,
     current_views BIGINT NOT NULL DEFAULT 0,
     current_earnings NUMERIC(12, 4) NOT NULL DEFAULT 0,
